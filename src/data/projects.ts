769c712860cdf7f09e3a027f2cca9acbb8f07c1a
Luis Gomez <luisgomez24g@gmail.com>
2025-06-03 22:18:08 -0500

    Segundo commit

---
+++ src/data/projects.ts
@@ -0,0 +1,74 @@
+export const PROJECTS = [
+  {
+    title: "Aldea",
+    description: "Software de planificación estratégica empresarial diseñado para centralizar la definición, seguimiento y análisis de objetivos organizacionales. Responsable del desarrollo frontend con Angular, enfocado en la integración de microservicios, arquitectura modular basada en componentes y diseño de interfaces centradas en la experiencia de usuario.",
+    image: "src/assets/projects_images/aldea.webp",
+    className: 'lg:row-span-4 lg:col-span-4',
+    experience: "Experiencia Laboral",
+    technologies: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "TypeScript",
+      "Angular",
+      "Angular Material"
+    ]
+  },
+  {
+    title: "Zona ser Afiliado",
+    description: "Oficina virtual de Mutualser EPS a través de la cual los afiliados y prestadores pueden gestionar sus servicios de salud de forma virtual. Encargado del mantenimiento de la aplicación frontend en la sección de portabilidad, que permite a los afiliados gestionar su cambio de EPS de manera sencilla y rápida.",
+    image: "src/assets/projects_images/zonaser.webp",
+    className: 'lg:row-span-3 lg:col-span-4',
+    experience: "Experiencia Laboral",
+    technologies: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "Vue.js"
+    ]
+  },
+  {
+    title: "Tronos",
+    description: "Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios.",
+    className: 'lg:row-span-2 lg:col-span-4',
+    experience: "Experiencia Laboral",
+    technologies: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "TypeScript",
+      "Angular",
+      "Angular Material"
+    ]
+  },
+  {
+    title: "PlayMatch",
+    description: "Aplicación web que permitirá registrar y consultar canchas sinteticas, hacer reservas y gestionar equipos. Está en desarrollo.",
+    image: "src/assets/projects_images/playmatch.webp",
+    className: 'lg:row-span-3 lg:col-span-4',
+    experience: "Experiencia Personal",
+    technologies: [
+      "HTML",
+      "CSS",
+      "Sass",
+      "TypeScript",
+      "Angular",
+      "Angular Material"
+    ]
+  },
+  {
+    title: "Agenda de Personas",
+    description: "Aplicación Full Stack desarrollada durante mi formación inicial. Implementa operaciones CRUD sobre un listado de personas, con backend en Spring Boot y frontend en Angular, utilizando Bootstrap para diseño responsivo.",
+    link: "https://github.com/LuisGomez11/api-angular-spring",
+    className: 'lg:row-span-2 lg:col-span-4',
+    experience: "Experiencia Personal",
+    technologies: [
+      "TypeScript",
+      "Angular",
+      "Bootstrap",
+      "Spring Boot",
+      "Java",
+      "MySQL"
+    ]
+  }
+];
---
+++ src/data/skills.ts
@@ -0,0 +1,50 @@
+export const SKILLS = [
+  {
+    name: "JavaScript",
+    icon: "src/assets/skills/js.webp",
+  },
+  {
+    name: "TypeScript",
+    icon: "src/assets/skills/ts.webp",
+  },
+  {
+    name: "Sass",
+    icon: "src/assets/skills/sass.webp",
+  },
+  {
+    name: "Tailwind CSS",
+    icon: "src/assets/skills/tailwind.webp",
+  },
+  {
+    name: "Angular",
+    icon: "src/assets/skills/angular.webp",
+  },
+  {
+    name: "Angular Material",
+    icon: "src/assets/skills/material.png",
+  },
+  {
+    name: "Astro",
+    icon: "src/assets/skills/astro.webp",
+  },
+  {
+    name: "Git",
+    icon: "src/assets/skills/git.webp",
+  },
+  {
+    name: "Java",
+    icon: "src/assets/skills/java.webp",
+  },
+  {
+    name: "Spring Boot",
+    icon: "src/assets/skills/spring.webp",
+  },
+  {
+    name: "Postman",
+    icon: "src/assets/skills/postman.webp",
+  },
+  {
+    name: "MySQL",
+    icon: "src/assets/skills/mysql.webp",
+  },
+];
