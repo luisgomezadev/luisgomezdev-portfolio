1fa6dbdb41957755cacf4cea226e60778b8eabf2
Luis Gomez <luisgomez24g@gmail.com>
2025-07-25 17:51:39 -0500

    Refactor all portfolio

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,88 +1,94 @@
 export const PROJECTS = [
   {
-    title: "Aldea",
-    description: "Software de planificación estratégica empresarial diseñado para centralizar la definición, seguimiento y análisis de objetivos organizacionales. Responsable del desarrollo frontend con Angular, enfocado en la integración de microservicios, arquitectura modular basada en componentes y diseño de interfaces centradas en la experiencia de usuario.",
-    image: "/projects_images/aldea.webp",
+    title: 'Aldea',
+    description:
+      'Software de planificación estratégica empresarial diseñado para centralizar la definición, seguimiento y análisis de objetivos organizacionales. Responsable del desarrollo frontend con Angular, enfocado en la integración de microservicios, arquitectura modular basada en componentes y diseño de interfaces centradas en la experiencia de usuario.',
+    image: '/projects_images/aldea.webp',
     className: 'lg:row-span-4 lg:col-span-4',
-    experience: "Experiencia Laboral",
+    experience: 'Experiencia Laboral',
     technologies: [
-      { name: "HTML", color: "text-orange-500" },
-      { name: "CSS", color: "text-blue-600" },
-      { name: "Sass", color: "text-pink-500" },
-      { name: "TypeScript", color: "text-blue-500" },
-      { name: "Angular", color: "text-red-600" },
-      { name: "Material", color: "text-yellow-600" }
-    ]
+      { name: 'HTML', color: 'text-orange-500' },
+      { name: 'CSS', color: 'text-blue-600' },
+      { name: 'Sass', color: 'text-pink-500' },
+      { name: 'TypeScript', color: 'text-blue-500' },
+      { name: 'Angular', color: 'text-red-600' },
+      { name: 'Material', color: 'text-yellow-600' },
+    ],
   },
   {
-    title: "Zona ser Afiliado",
-    description: "Oficina virtual de Mutualser EPS a través de la cual los afiliados y prestadores pueden gestionar sus servicios de salud de forma virtual. Encargado del mantenimiento de la aplicación frontend en la sección de portabilidad, que permite a los afiliados gestionar su cambio de EPS de manera sencilla y rápida.",
-    image: "/projects_images/zonaser.webp",
+    title: 'Zona ser Afiliado',
+    description:
+      'Oficina virtual de Mutualser EPS a través de la cual los afiliados y prestadores pueden gestionar sus servicios de salud de forma virtual. Encargado del mantenimiento de la aplicación frontend en la sección de portabilidad, que permite a los afiliados gestionar su cambio de EPS de manera sencilla y rápida.',
+    image: '/projects_images/zonaser.webp',
     className: 'lg:row-span-3 lg:col-span-4',
-    experience: "Experiencia Laboral",
+    experience: 'Experiencia Laboral',
     technologies: [
-      { name: "HTML", color: "text-orange-500" },
-      { name: "CSS", color: "text-blue-600" },
-      { name: "Sass", color: "text-pink-500" },
-      { name: "Vue.js", color: "text-emerald-500" }
-    ]
+      { name: 'HTML', color: 'text-orange-500' },
+      { name: 'CSS', color: 'text-blue-600' },
+      { name: 'Sass', color: 'text-pink-500' },
+      { name: 'Vue.js', color: 'text-emerald-500' },
+    ],
   },
   {
-    title: "PlayMatch",
-    description: "Aplicación desarrolada con Spring Boot y Angular que permite registrar y consultar canchas sinteticas, hacer reservas, entre otras cosas. Esta aplicación es parte de un proyecto personal que busca facilitar la organización de reservas de las canchas.",
-    image: "/projects_images/playm.webp",
-    link: "https://github.com/luisgomezadev/Backend-PlayMatch",
+    title: 'PlayMatch',
+    description:
+      'Aplicación desarrolada con Spring Boot y Angular que permite registrar y consultar canchas sinteticas, hacer reservas, entre otras cosas. Esta aplicación es parte de un proyecto personal que busca facilitar la organización de reservas de las canchas.',
+    image: '/projects_images/playm.webp',
+    link: 'https://github.com/luisgomezadev/Backend-PlayMatch',
     live: 'https://playmatch-lgsoftworks.vercel.app/',
     className: 'lg:row-span-4 lg:col-span-4',
-    experience: "Experiencia Personal",
+    experience: 'Experiencia Personal',
     technologies: [
-      { name: "Java", color: "text-[#007396]" }, // color específico de Java
-      { name: "Spring Boot", color: "text-green-600" },
-      { name: "Spring Security", color: "text-green-700" },
-      { name: "MySQL", color: "text-cyan-700" },
-      { name: "Angular", color: "text-red-600" },
-      { name: "TailwindCSS", color: "text-sky-400" }
-    ]
+      { name: 'Java', color: 'text-[#007396]' }, // color específico de Java
+      { name: 'Spring Boot', color: 'text-green-600' },
+      { name: 'Spring Security', color: 'text-green-700' },
+      { name: 'MySQL', color: 'text-cyan-700' },
+      { name: 'Angular', color: 'text-red-600' },
+      { name: 'TailwindCSS', color: 'text-sky-400' },
+    ],
   },
+  // {
+  //   title: 'Tronos',
+  //   description:
+  //     'Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios y corrección de bugs y mejoras.',
+  //   className: 'lg:row-span-2 lg:col-span-4',
+  //   experience: 'Experiencia Laboral',
+  //   technologies: [
+  //     { name: 'HTML', color: 'text-orange-500' },
+  //     { name: 'CSS', color: 'text-blue-600' },
+  //     { name: 'Sass', color: 'text-pink-500' },
+  //     { name: 'TypeScript', color: 'text-blue-500' },
+  //     { name: 'Angular', color: 'text-red-600' },
+  //     { name: 'Material', color: 'text-yellow-600' },
+  //   ],
+  // },
   {
-    title: "Tronos",
-    description: "Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios y corrección de bugs y mejoras.",
-    className: 'lg:row-span-2 lg:col-span-4',
-    experience: "Experiencia Laboral",
-    technologies: [
-      { name: "HTML", color: "text-orange-500" },
-      { name: "CSS", color: "text-blue-600" },
-      { name: "Sass", color: "text-pink-500" },
-      { name: "TypeScript", color: "text-blue-500" },
-      { name: "Angular", color: "text-red-600" },
-      { name: "Material", color: "text-yellow-600" }
-    ]
-  },
-  {
-    title: "Landing Page | Daniel Kelly",
-    description: "Landing page responsive para un entrenador personal, desarrollada con Astro y Tailwind CSS. Presenta una estructura clara y moderna que destaca los servicios, eventos, entre otras cosas, optimizando el rendimiento, accesibilidad y velocidad de carga. El proyecto se enfoca en ofrecer una experiencia visual atractiva y profesional, con código limpio, semántico y fácil de escalar.",
-    image: "/projects_images/landing.webp",
+    title: 'Landing Page | Daniel Kelly',
+    description:
+      'Landing page responsive para un entrenador personal, desarrollada con Astro y Tailwind CSS. Presenta una estructura clara y moderna que destaca los servicios, eventos, entre otras cosas, optimizando el rendimiento, accesibilidad y velocidad de carga.',
+    image: '/projects_images/landing.webp',
     live: 'https://danielkellyfit.vercel.app/',
-    link: "https://github.com/luisgomezadev/danielkellyfit-page",
+    link: 'https://github.com/luisgomezadev/danielkellyfit-page',
     className: 'lg:row-span-4 lg:col-span-4',
-    experience: "Experiencia Personal",
+    experience: 'Experiencia Personal',
     technologies: [
-      { name: "Astro", color: "text-purple-500" },
-      { name: "TailwindCSS", color: "text-sky-400" }
-    ]
+      { name: 'Astro', color: 'text-purple-500' },
+      { name: 'TailwindCSS', color: 'text-sky-400' },
+    ],
   },
   {
-    title: "Doon",
-    description: "Aplicación web desarrollada con Vue.js 3 y estilizada con Tailwind CSS, que permite gestionar tareas de forma intuitiva y rápida. Diseñada con un enfoque minimalista y moderno, utiliza localStorage para almacenar la información localmente en el navegador.",
-    image: "/projects_images/doon.webp",
+    title: 'Doon',
+    description:
+      'Aplicación web desarrollada con Vue.js 3 y estilizada con Tailwind CSS, que permite gestionar tareas de forma intuitiva y rápida. Diseñada con un enfoque minimalista y moderno, utiliza localStorage para almacenar la información localmente en el navegador.',
+    image: '/projects_images/doon.webp',
     live: 'https://doon-teal.vercel.app/',
-    link: "https://github.com/luisgomezadev/doon",
+    link: 'https://github.com/luisgomezadev/doon',
     className: 'lg:row-span-3 lg:col-span-4',
-    experience: "Experiencia Personal",
+    experience: 'Experiencia Personal',
     technologies: [
-      { name: "TypeScript", color: "text-blue-500" },
-      { name: "VueJS", color: "text-emerald-500" },
-      { name: "TailwindCSS", color: "text-sky-400" }
-    ]
+      { name: 'TypeScript', color: 'text-blue-500' },
+      { name: 'VueJS', color: 'text-emerald-500' },
+      { name: 'TailwindCSS', color: 'text-sky-400' },
+    ],
   },
-];
+]
--- src/data/skills.ts
+++ src/data/skills.ts
@@ -1,50 +1,50 @@
 export const SKILLS = [
   {
-    name: "JavaScript",
-    icon: "/skills/js.webp",
+    name: 'JavaScript',
+    icon: '/skills/js.webp',
   },
   {
-    name: "TypeScript",
-    icon: "/skills/ts.webp",
+    name: 'TypeScript',
+    icon: '/skills/ts.webp',
   },
   {
-    name: "Sass",
-    icon: "/skills/sass.webp",
+    name: 'Sass',
+    icon: '/skills/sass.webp',
   },
   {
-    name: "Tailwind CSS",
-    icon: "/skills/tailwind.webp",
+    name: 'Tailwind CSS',
+    icon: '/skills/tailwind.webp',
   },
   {
-    name: "Angular",
-    icon: "/skills/angular.webp",
+    name: 'Angular',
+    icon: '/skills/angular.webp',
   },
   {
-    name: "VueJS",
-    icon: "/skills/vue.webp",
+    name: 'VueJS',
+    icon: '/skills/vue.webp',
   },
   {
-    name: "Astro",
-    icon: "/skills/astro.webp",
+    name: 'Astro',
+    icon: '/skills/astro.webp',
   },
   {
-    name: "Git",
-    icon: "/skills/git.webp",
+    name: 'Git',
+    icon: '/skills/git.webp',
   },
   {
-    name: "Java",
-    icon: "/skills/java.webp",
+    name: 'Java',
+    icon: '/skills/java.webp',
   },
   {
-    name: "Spring Boot",
-    icon: "/skills/spring.webp",
+    name: 'Spring Boot',
+    icon: '/skills/spring.webp',
   },
   {
-    name: "Postman",
-    icon: "/skills/postman.webp",
+    name: 'Postman',
+    icon: '/skills/postman.webp',
   },
   {
-    name: "MySQL",
-    icon: "/skills/mysql.webp",
+    name: 'MySQL',
+    icon: '/skills/mysql.webp',
   },
-];
+]
