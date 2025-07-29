90da7c4fc0ca8d708302fd58723434e95ea58728
Luis Gomez <luisgomez24g@gmail.com>
2025-07-29 15:18:19 -0500

    Add projets page

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,9 +3,10 @@ export const PROJECTS = [
     title: 'Aldea',
     description:
       'Software de planificación estratégica empresarial diseñado para centralizar la definición, seguimiento y análisis de objetivos organizacionales. Responsable del desarrollo frontend con Angular, enfocado en la integración de microservicios, arquitectura modular basada en componentes y diseño de interfaces centradas en la experiencia de usuario.',
-    image: '/projects_images/aldea.webp',
+    image: '/projects_images/icon/aldea.webp',
     className: 'lg:row-span-4 lg:col-span-4',
     experience: 'Experiencia Laboral',
+    home: true,
     technologies: [
       { name: 'HTML', color: 'text-orange-500' },
       { name: 'CSS', color: 'text-blue-600' },
@@ -19,9 +20,10 @@ export const PROJECTS = [
     title: 'Zona ser Afiliado',
     description:
       'Oficina virtual de Mutualser EPS a través de la cual los afiliados y prestadores pueden gestionar sus servicios de salud de forma virtual. Encargado del mantenimiento de la aplicación frontend en la sección de portabilidad, que permite a los afiliados gestionar su cambio de EPS de manera sencilla y rápida.',
-    image: '/projects_images/zonaser.webp',
+    image: '/projects_images/icon/zonaser.webp',
     className: 'lg:row-span-3 lg:col-span-4',
     experience: 'Experiencia Laboral',
+    home: true,
     technologies: [
       { name: 'HTML', color: 'text-orange-500' },
       { name: 'CSS', color: 'text-blue-600' },
@@ -30,14 +32,32 @@ export const PROJECTS = [
     ],
   },
   {
+    title: 'Tronos',
+    description:
+      'Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios y corrección de bugs y mejoras.',
+    image: '/projects_images/icon/tronos.webp',
+    className: 'lg:row-span-2 lg:col-span-4',
+    experience: 'Experiencia Laboral',
+    home: false,
+    technologies: [
+      { name: 'HTML', color: 'text-orange-500' },
+      { name: 'CSS', color: 'text-blue-600' },
+      { name: 'Sass', color: 'text-pink-500' },
+      { name: 'TypeScript', color: 'text-blue-500' },
+      { name: 'Angular', color: 'text-red-600' },
+      { name: 'Material', color: 'text-yellow-600' },
+    ],
+  },
+  {
     title: 'PlayMatch',
     description:
       'Aplicación desarrolada con Spring Boot y Angular que permite registrar y consultar canchas sinteticas, hacer reservas, entre otras cosas. Esta aplicación es parte de un proyecto personal que busca facilitar la organización de reservas de las canchas.',
-    image: '/projects_images/playm.webp',
-    link: 'https://github.com/luisgomezadev/Backend-PlayMatch',
+    image: '/projects_images/icon/playmatch.webp',
+    link: 'https://github.com/luisgomezadev/frontend-playmatch',
     live: 'https://playmatch-lgsoftworks.vercel.app/',
     className: 'lg:row-span-4 lg:col-span-4',
     experience: 'Experiencia Personal',
+    home: true,
     technologies: [
       { name: 'Java', color: 'text-[#007396]' }, // color específico de Java
       { name: 'Spring Boot', color: 'text-green-600' },
@@ -47,30 +67,16 @@ export const PROJECTS = [
       { name: 'TailwindCSS', color: 'text-sky-400' },
     ],
   },
-  // {
-  //   title: 'Tronos',
-  //   description:
-  //     'Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios y corrección de bugs y mejoras.',
-  //   className: 'lg:row-span-2 lg:col-span-4',
-  //   experience: 'Experiencia Laboral',
-  //   technologies: [
-  //     { name: 'HTML', color: 'text-orange-500' },
-  //     { name: 'CSS', color: 'text-blue-600' },
-  //     { name: 'Sass', color: 'text-pink-500' },
-  //     { name: 'TypeScript', color: 'text-blue-500' },
-  //     { name: 'Angular', color: 'text-red-600' },
-  //     { name: 'Material', color: 'text-yellow-600' },
-  //   ],
-  // },
   {
     title: 'Landing Page | Daniel Kelly',
     description:
       'Landing page responsive para un entrenador personal, desarrollada con Astro y Tailwind CSS. Presenta una estructura clara y moderna que destaca los servicios, eventos, entre otras cosas, optimizando el rendimiento, accesibilidad y velocidad de carga.',
-    image: '/projects_images/landing.webp',
+    image: '/projects_images/icon/danielkelly.webp',
     live: 'https://danielkellyfit.vercel.app/',
     link: 'https://github.com/luisgomezadev/danielkellyfit-page',
     className: 'lg:row-span-4 lg:col-span-4',
     experience: 'Experiencia Personal',
+    home: true,
     technologies: [
       { name: 'Astro', color: 'text-purple-500' },
       { name: 'TailwindCSS', color: 'text-sky-400' },
@@ -80,11 +86,12 @@ export const PROJECTS = [
     title: 'Doon',
     description:
       'Aplicación web desarrollada con Vue.js 3 y estilizada con Tailwind CSS, que permite gestionar tareas de forma intuitiva y rápida. Diseñada con un enfoque minimalista y moderno, utiliza localStorage para almacenar la información localmente en el navegador.',
-    image: '/projects_images/doon.webp',
+    image: '/projects_images/icon/doon.webp',
     live: 'https://doon-teal.vercel.app/',
     link: 'https://github.com/luisgomezadev/doon',
     className: 'lg:row-span-3 lg:col-span-4',
     experience: 'Experiencia Personal',
+    home: false,
     technologies: [
       { name: 'TypeScript', color: 'text-blue-500' },
       { name: 'VueJS', color: 'text-emerald-500' },
