3c3d5a877fc7c480cbc5d57b7500179d8e3f8eb5
Luis Gomez <luisgomez24g@gmail.com>
2025-08-15 00:12:12 -0500

    Refactor experience and projects images

---
+++ src/data/projects-experience.ts
@@ -0,0 +1,44 @@
+export const PROJECTS_EXPERIENCE = [
+  {
+    id: 'aldea',
+    title: 'Aldea',
+    description:
+      'Software de planificación estratégica empresarial diseñado para centralizar la definición, seguimiento y análisis de objetivos organizacionales. Responsable del desarrollo frontend con Angular, enfocado en la integración de microservicios, arquitectura modular basada en componentes y diseño de interfaces centradas en la experiencia de usuario.',
+    technologies: [
+      "/skills/html.webp",
+      "/skills/css.webp",
+      "/skills/sass.webp",
+      "/skills/ts.webp",
+      "/skills/angular.webp",
+      "/skills/material.png",
+    ],
+  },
+  {
+    id: 'zonaser',
+    title: 'Zona ser Afiliado',
+    description:
+      'Oficina virtual de Mutualser EPS a través de la cual los afiliados y prestadores pueden gestionar sus servicios de salud de forma virtual. Encargado del mantenimiento de la aplicación frontend en la sección de portabilidad, que permite a los afiliados gestionar su cambio de EPS de manera sencilla y rápida.',
+    technologies: [
+      "/skills/html.webp",
+      "/skills/css.webp",
+      "/skills/js.webp",
+      "/skills/sass.webp",
+      "/skills/vue.webp",
+    ],
+  },
+  {
+    id: 'tronos',
+    title: 'Tronos',
+    description:
+      'Software que permite manejar los procesos académicos, como la gestión de estudiantes, docentes, notas y otros aspectos relacionados, en la universidad Unicolombo. Encargado del desarrollo frontend con Angular, enfocado en la integración de microservicios y corrección de bugs y mejoras.',
+    technologies: [
+      "/skills/html.webp",
+      "/skills/css.webp",
+      "/skills/sass.webp",
+      "/skills/ts.webp",
+      "/skills/angular.webp",
+      "/skills/material.png",
+      "/skills/spring.webp",
+    ],
+  }
+]
