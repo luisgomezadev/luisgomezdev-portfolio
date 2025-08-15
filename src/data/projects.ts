d1d7ffdb2d786b61643ca5828534a34d3e05c9e5
Luis Gomez <luisgomez24g@gmail.com>
2025-08-15 00:31:53 -0500

    optimized images

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,7 +3,7 @@ export const PROJECTS = [
     title: 'PlayMatch',
     description:
       'Aplicación desarrolada con Spring Boot y Angular que permite registrar y consultar canchas sinteticas, hacer reservas, entre otras cosas. Esta aplicación es parte de un proyecto personal que busca facilitar la organización de reservas de las canchas.',
-    image: '/projects_images/playmatch.png',
+    image: '/projects_images/playmatch.webp',
     link: 'https://github.com/luisgomezadev/frontend-playmatch',
     live: 'https://playmatch-lgsoftworks.vercel.app/',
     className: 'lg:row-span-4 lg:col-span-4',
@@ -22,7 +22,7 @@ export const PROJECTS = [
     title: 'Landing Page | Daniel Kelly',
     description:
       'Landing page responsive para un entrenador personal, desarrollada con Astro y Tailwind CSS. Presenta una estructura clara y moderna que destaca los servicios, eventos, entre otras cosas, optimizando el rendimiento, accesibilidad y velocidad de carga.',
-    image: '/projects_images/daniel.png',
+    image: '/projects_images/daniel.webp',
     live: 'https://danielkellyfit.vercel.app/',
     link: 'https://github.com/luisgomezadev/danielkellyfit-page',
     className: 'lg:row-span-4 lg:col-span-4',
@@ -37,7 +37,7 @@ export const PROJECTS = [
     title: 'Doon',
     description:
       'Aplicación web desarrollada con Vue.js 3 y estilizada con Tailwind CSS, que permite gestionar tareas de forma intuitiva y rápida. Diseñada con un enfoque minimalista y moderno, utiliza localStorage para almacenar la información localmente en el navegador.',
-    image: '/projects_images/doon.png',
+    image: '/projects_images/doon.webp',
     live: 'https://doon-teal.vercel.app/',
     link: 'https://github.com/luisgomezadev/doon',
     className: 'lg:row-span-3 lg:col-span-4',
